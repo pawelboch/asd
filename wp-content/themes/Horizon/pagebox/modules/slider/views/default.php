f69854f1a2c5e6039d2463eec77483fd3283241b
Tomasz Pach <tomaszpach@icloud.com>
2016-02-06 11:36:02 +0100

    SCSS update || Module update

--- wp-content/themes/Horizon/pagebox/modules/slider/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/slider/views/default.php
@@ -5,9 +5,11 @@
 ?>
 
 	<div class="container slider-module">
-		<h2> <?php echo $this->get( 'title' ) ;?> </h2>
-		<p> <?php echo $this->get( 'description' ) ;?> </p>
 
-		<a href="<?php echo $this->get( 'button' );?>" class="btn-2">learn more ></a>
+			<h2> <?php echo $this->get( 'title' ) ;?> </h2>
+			<p> <?php echo $this->get( 'description' ) ;?> </p>
+
+			<a href="<?php echo $this->get( 'button' );?>" class="btn-2">learn more ></a>
+
 	</div>
 

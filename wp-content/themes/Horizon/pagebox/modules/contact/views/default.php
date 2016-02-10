893520cbd323a6aa15ebe1819e693b6bfc713782
Tomasz Pach <tomaszpach@icloud.com>
2016-02-10 16:05:57 +0100

    contact module

---
+++ wp-content/themes/Horizon/pagebox/modules/contact/views/default.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Text view
+ */
+?>
+
+	<div class="container contact-module-fg23gh">
+		<div class="col-md-6 left-part">
+			<h2 class="title">
+				Get in touch
+			</h2>
+
+			<p class="description">
+				Nullam eu ultricies lacus. Aliquam nec lacinia justo. Phasellus tempus consequat ex non fringilla. Integer tincidunt dui non mollis elementum.
+			</p>
+
+		</div>
+		<div class="col-md-6 contact-form">
+			<div class="first-name">
+				first name
+			</div>
+
+			<div class="your-email">
+				your email
+			</div>
+
+			<div class="investor">
+				investor
+			</div>
+			<div class="message">
+				message
+			</div>
+		</div>
+	</div>
+
--- wp-content/themes/Horizon/pagebox/modules/text/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/text/views/default.php
@@ -8,7 +8,7 @@
 		<div class="border clearfix">
 			<div class="col-md-10">
 				<?php echo $this->get( 'text' ) ;?>
-				<a href="<?php echo $this->get( 'button' );?>" class="learn-more-btn col-xs-12 col-sm-4 btn-2">learn more<span> > </span></a>
+				<a href="<?php echo $this->get( 'button' );?>" class="col-xs-12 col-md-3 learn-more-btn">learn more<span> > </span></a>
 			</div>
 		</div>
 	</div>

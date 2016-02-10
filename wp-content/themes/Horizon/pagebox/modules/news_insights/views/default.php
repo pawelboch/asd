54e4b921ac89a57c12dc9a6a48cd08075e60bccd
Tomasz Pach <tomaszpach@icloud.com>
2016-02-10 12:13:09 +0100

    news_insights

---
+++ wp-content/themes/Horizon/pagebox/modules/news_insights/views/default.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * two_images view
+ */
+?>
+
+	<div class="container two_images-module-fg23gh">
+
+		<div class="half-part left col-md-6">
+			<div class="row">
+				<h2 class="title">
+					<?php echo $this->get( 'title_left' ) ;?>
+				</h2>
+
+				<p class="description"><?php echo $this->get( 'description_left' ) ;?></p>
+
+				<a href="#" class="learn-more-btn col-xs-12 btn-2">
+					learn more<span> > </span>
+				</a>
+			</div>
+		</div>
+
+		<div class="half-part right col-md-6">
+			<div class="row">
+				<h2 class="title">
+					<?php echo $this->get( 'title_right' ) ;?>
+				</h2>
+
+				<p class="description"><?php echo $this->get( 'description_right' ) ;?></p>
+
+				<a href="#" class="learn-more-btn col-xs-12 btn-2">
+					learn more<span> > </span>
+				</a>
+			</div>
+		</div>
+
+	</div>
+

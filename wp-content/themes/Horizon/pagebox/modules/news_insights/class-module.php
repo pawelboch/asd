b7ec75c241026c1a9200c7b03f0831003982b288
Tomasz Pach <tomaszpach@icloud.com>
2016-02-10 12:16:52 +0100

    news_insights fix

--- wp-content/themes/Horizon/pagebox/modules/news_insights/class-module.php
+++ wp-content/themes/Horizon/pagebox/modules/news_insights/class-module.php
@@ -7,7 +7,7 @@
  * Call to action module with slider
  */
 
-namespace HorizonInvestments\Pagebox\Modules\two_images;
+namespace HorizonInvestments\Pagebox\Modules\News_insights;
 
 use \Pagebox\Modules\Module as Abstract_Module;
 use \WPGeeks_HTML;
@@ -32,10 +32,10 @@ class Module extends Abstract_Module {
 		$this->config = array(
 			// Name of the box for plugin use. Only alphanumeric charactes 
 			// and underscores are allowed
-			'slug'        => 'two_images',
+			'slug'        => 'news_insights',
 			// Human readable title of box. It will be displayed in all
 			// backend functionalities
-			'title'       => __(  'Two images', 'pagebox_blocks' ),
+			'title'       => __(  'News & Insisghts', 'pagebox_blocks' ),
 			// Short description about what box outputs. It will be displayed
 			// below the title in new box modal window.
 			'description' => __(  '', 'pagebox_blocks' ),

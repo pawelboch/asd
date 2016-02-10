7c1d8e5163660da8bbf1adcce74d53225b6d39cb
Tomasz Pach <tomaszpach@icloud.com>
2016-02-10 15:17:42 +0100

    module variables cleanup v2

--- wp-content/themes/Horizon/pagebox/modules/text/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/text/views/default.php
@@ -8,7 +8,7 @@
 		<div class="border clearfix">
 			<div class="col-md-10">
 				<?php echo $this->get( 'text' ) ;?>
-				<a class="col-xs-12 learn-more-btn" href="<?php echo $this->get( 'button' );?>">learn more <span> > </span></a>
+				<a href="<?php echo $this->get( 'button' );?>" class="learn-more-btn col-xs-12 col-sm-4 btn-2">learn more<span> > </span></a>
 			</div>
 		</div>
 	</div>

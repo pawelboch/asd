53f06286a276bad2c16dc2bc8afbc0b09cd044cb
Tomasz Pach <tomaszpach@icloud.com>
2016-02-13 13:45:57 +0100

    Single image module

---
+++ wp-content/themes/Horizon/pagebox/modules/one_image/class-module.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Module class is an extension for Pagebox\Module
+ * abstract class. It registeres new box into
+ * Pagebox plugin
+ *
+ * Call to action module with slider
+ */
+
+namespace HorizonInvestments\Pagebox\Modules\one_image;
+
+use \Pagebox\Modules\Module as Abstract_Module;
+use \WPGeeks_HTML;
+
+class Module extends Abstract_Module {
+
+	/**
+	 * Module constructor
+	 * @access public
+	 */
+	public function __construct() {
+
+		parent::__construct();
+
+	}
+	/**
+	 * Template config
+	 * @return void
+	 */
+	public function set_config() {
+
+		$this->config = array(
+			// Name of the box for plugin use. Only alphanumeric charactes 
+			// and underscores are allowed
+			'slug'        => 'one_image',
+			// Human readable title of box. It will be displayed in all
+			// backend functionalities
+			'title'       => __(  'One image', 'pagebox_blocks' ),
+			// Short description about what box outputs. It will be displayed
+			// below the title in new box modal window.
+			'description' => __(  '', 'pagebox_blocks' ),
+			// Elements with higher priority will be displayed earlier in
+			// new box modal window
+			'priority'    => 95,
+			// Custom path for image file. Default image (module.jpg) should be
+			// located in the main folder of current box
+			'image'       => '',
+			'limit'       => array(
+				'width'    => array( 0, 100 )
+			),
+			// minimum and maximum percent width that module fits in
+			// WPGeeks_Forms
+			'fields'      => array(
+				array(
+						'type'			=> 'image',
+						'group'			=> __( 'General', 'pagebox'),
+						'name'			=> 'image',
+						'option'	  	=> 'yes',
+						'label'			=> __( 'Image to display' ),
+						'description'	=> __( 'Select image to display on page' )
+				),
+			)
+		);
+	}
+}
---
+++ wp-content/themes/Horizon/pagebox/modules/one_image/views/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * one_image view
+ */
+?>
+
+	<div class="container one_image-module-fg23gh" style="background-image: url(<?php echo wp_get_attachment_url($this->get('image')) ;?>)">
+
+
+	</div>
+

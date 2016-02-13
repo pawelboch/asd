87586e90e9928abd67c9bcb1599d0d545d3d488f
Tomasz Pach <tomaszpach@icloud.com>
2016-02-13 19:07:47 +0100

    single.php || style

--- wp-content/themes/Horizon/single.php
+++ wp-content/themes/Horizon/single.php
@@ -6,48 +6,56 @@
 		<h2 class="title"><?php the_title() ;?></h2>
 	</div>
 
-	<div class="sidebar col-md-2">
-		<div class="author">
-			<div class="name">
-				Jon Jon
+	<div class="content col-md-12">
+		<div class="row">
+
+			<div class="sidebar col-md-2">
+				<div class="author">
+					<div class="name">
+						Jon Jon
+					</div>
+
+					<div class="position">
+						Asset Menager
+					</div>
+				</div>
+
+				<div class="details">
+					<div class="published">
+						Published on:
+					</div>
+
+					<div class="date">
+						12.12.2012
+					</div>
+				</div>
+
+				<div class="share-print">
+					<span class="social">
+						<i class="fa fa-share-alt"></i>
+						share
+					</span>
+
+					<span class="print">
+						print
+					</span>
+				</div>
 			</div>
 
-			<div class="position">
-				Asset Menager
+			<div class="post-text col-md-10">
+				<?php
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post();
+						the_content();
+					}
+				}
+				?>
 			</div>
-		</div>
 
-		<div class="details">
-			<div class="title">
-				Published on
-			</div>
-
-			<div class="date">
-				<?php the_date('Y-m-d'); ?>
-			</div>
-		</div>
-
-		<div class="share_print">
-			<span class="social">
-				share
-			</span>
-
-			<span class="print">
-				print
-			</span>
 		</div>
 	</div>
 
-	<div class="content col-md-10">
-		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post();
-				the_content();
-			}
-		}
-		?>
-	</div>
 
 </div>
 

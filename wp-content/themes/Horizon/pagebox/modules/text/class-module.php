2e8540743579b6dd227fae801eb937c631f6c826
Tomasz Pach <tomaszpach@icloud.com>
2016-02-13 14:32:40 +0100

    text module some if update

--- wp-content/themes/Horizon/pagebox/modules/text/class-module.php
+++ wp-content/themes/Horizon/pagebox/modules/text/class-module.php
@@ -53,20 +53,41 @@ class Module extends Abstract_Module {
 			'fields'      => array(
 				array(
 						'type'			=> 'editor',
-						'group'			=> __( 'main', 'pagebox'),
+						'group'			=> __( 'Main', 'pagebox'),
 						'name'			=> 'text',
 						'option'	  	=> 'yes',
-						'label'			=> __( 'eee' ),
-						'description'	=> __( 'sss')
+						'label'			=> __( 'Editor' ),
+						'description'	=> __( 'Type your text')
 				),
 				array(
 					'type'			=> 'SinglePage',
-					'group'			=> __( 'main', 'pagebox'),
+					'group'			=> __( 'Main', 'pagebox'),
 					'name'			=> 'button',
 					'option'	  	=> 'yes',
-					'label'			=> __( 'eee' ),
-					'description'	=> __( 'sss')
-				)
+					'label'			=> __( 'Select page' ),
+					'description'	=> __( 'Select page for button')
+				),
+				array(
+					'type'			=> 'switch',
+					'group'			=> __( 'Style', 'pagebox' ),
+					'name'			=> 'btn_switch',
+					'label'			=> __( 'learn more btn switch' ),
+					'description'	=> __( 'lrn mor btn')
+				),
+				array(
+					'type'			=> 'switch',
+					'group'			=> __( 'Style', 'pagebox' ),
+					'name'			=> 'border_switch',
+					'label'			=> __( 'border switch' ),
+					'description'	=> __( 'border')
+				),
+				array(
+					'type'			=> 'text',
+					'group'			=> __( 'Style', 'pagebox' ),
+					'name'			=> 'border_style',
+					'label'			=> __( 'Border style' ),
+					'description'	=> __( 'Leave empty for default border style')
+				),
 			)
 		);
 	}
--- wp-content/themes/Horizon/pagebox/modules/text/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/text/views/default.php
@@ -4,12 +4,25 @@
  */
 ?>
 
-	<div class="container text-module-fg23gh">
-		<div class="border clearfix">
-			<div class="col-md-10">
-				<?php echo $this->get( 'text' ) ;?>
+<div class="container text-module-fg23gh">
+
+<?php $border = $this->get('border_switch');
+	if($border !== false) : ?>
+		<div class="border clearfix" style="border-bottom: <?php echo $this->get('border_style') ;?>">
+	<?php else: ;?>
+		<div class="border clearfix" style="border: none">
+	<?php endif ;?>
+
+		<div class="col-md-10">
+
+			<?php echo $this->get( 'text' ) ;?>
+
+			<?php if($this->get('btn_switch') !== false): ?>
 				<a href="<?php echo $this->get( 'button' );?>" class="col-xs-12 col-md-3 learn-more-btn">learn more<span> > </span></a>
-			</div>
+			<?php else: ;?>
+				<!-- btn off -->
+			<?php endif ;?>
+
 		</div>
 	</div>
-
+</div>

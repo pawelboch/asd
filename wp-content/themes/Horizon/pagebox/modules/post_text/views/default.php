982e397f541b8c65bf0df0755f4dbc0e72f1ba21
Tomasz Pach <tomaszpach@icloud.com>
2016-02-18 15:28:00 +0100

    post_text module

---
+++ wp-content/themes/Horizon/pagebox/modules/post_text/views/default.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Post_info view
+ */
+?>
+
+<?php global $post; ?>
+
+<div class="<?php echo $this->get('css_class'); ?>">
+    <div class="author">
+        <div class="name">
+            <?php
+
+            $author = get_user_meta( $post->post_author );
+
+            echo $author["first_name"][0] . ' ' . $author["last_name"][0];
+
+            ?>
+        </div>
+
+        <div class="description">
+            <?php echo $author["description"][0] ;?>
+        </div>
+    </div>
+
+
+
+    <div class="published">
+        <div class="text">
+            Published on:
+        </div>
+
+        <div class="date">
+            <?php echo get_the_time('j.m.Y',$post->ID) ;?>
+        </div>
+    </div>
+
+    <div class="share">
+        <span class="social">
+            social
+        </span>
+        <span class="print">
+            print
+        </span>
+    </div>
+</div>
+

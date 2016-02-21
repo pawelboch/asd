a377ad6e97e87766051e338add389ea50c3c3274
Piotr Grzesiak <piter2k1@gmail.com>
2016-02-21 22:24:46 +0100

    Recompile theme & modules

--- wp-content/plugins/pagebox/src/public/partials/sass/compiler.php
+++ wp-content/plugins/pagebox/src/public/partials/sass/compiler.php
@@ -4,7 +4,7 @@
 	<p>
 		<?php if( $permission_test ): ?>
 		<a href="<?php echo admin_url( 'admin.php?page=pagebox-sass-compiler&task=recompile' ); ?>" onclick="return confirm('Are You sure?');" class="button button-primary">Recompile & minify</a>
-		<a href="<?php echo admin_url( 'admin.php?page=pagebox-sass-compiler&task=recompile_theme' ); ?>" onclick="return confirm('Are You sure?');" class="button button-secondary">Recompile theme (with bootstrap)</a>
+		<a href="<?php echo admin_url( 'admin.php?page=pagebox-sass-compiler&task=recompile_theme' ); ?>" onclick="return confirm('Are You sure?');" class="button">Recompile theme (with bootstrap)</a>
 		<a href="<?php echo admin_url( 'admin.php?page=pagebox-sass-compiler&task=recompile_modules' ); ?>" onclick="return confirm('Are You sure?');" class="button">Recompile pagebox modules</a>
 		<a href="<?php echo admin_url( 'admin.php?page=pagebox-sass-compiler&task=minify' ); ?>" onclick="return confirm('Are You sure?');" class="button">Concatenate and minify</a>
 		<?php else: ?>

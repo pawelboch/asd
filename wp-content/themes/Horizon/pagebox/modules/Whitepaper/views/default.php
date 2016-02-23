2bcdf5e5ef37894979c4f563a0c72e5f5c03a601
Tomasz Pach <tomaszpach@icloud.com>
2016-02-23 15:03:58 +0100

    PDF module v3

--- wp-content/themes/Horizon/pagebox/modules/Whitepaper/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/Whitepaper/views/default.php
@@ -7,57 +7,45 @@
 	<div class="container <?php echo $this->get('css_class'); ?>">
 		<div class="col-md-4 single">
 			<div class="title">
-				Whitepaper:
-				Top 10 Investment
-				proposals for 2016
+				<?php echo $this->get('title1') ;?>
 			</div>
 			<div class="description">
 				Nullam eu ultricies lacus. Aliquam nec lacinia justo. Phasellus tempus consequat ex non fringilla...
 			</div>
 			<div class="download">
 				<a href="#">
-					<div class="ico">
-
-					</div>
-					<span class="text"> download </span>
+					<div class="ico"></div>
+					<span class="text"> <?php echo $this->get('download_btn') ;?> </span>
 				</a>
 			</div>
 		</div>
 
 		<div class="col-md-4 single">
 			<div class="title">
-				Whitepaper:
-				Top 10 Investment
-				proposals for 2016
+				<?php echo $this->get('title2') ;?>
 			</div>
 			<div class="description">
 				Nullam eu ultricies lacus. Aliquam nec lacinia justo. Phasellus tempus consequat ex non fringilla...
 			</div>
 			<a href="#">
 				<div class="download">
-					<div class="ico">
-
-					</div>
-					<span class="text"> download </span>
+					<div class="ico"></div>
+					<span class="text"> <?php echo $this->get('download_btn') ;?> </span>
 				</div>
 			</a>
 		</div>
 
 		<div class="col-md-4 single">
 			<div class="title">
-				Whitepaper:
-				Top 10 Investment
-				proposals for 2016
+				<?php echo $this->get('title3') ;?>
 			</div>
 			<div class="description">
 				Nullam eu ultricies lacus. Aliquam nec lacinia justo. Phasellus tempus consequat ex non fringilla...
 			</div>
 			<a href="#">
 				<div class="download">
-					<div class="ico">
-
-					</div>
-					<span class="text"> download </span>
+					<div class="ico"></div>
+					<span class="text"> <?php echo $this->get('download_btn') ;?> </span>
 				</div>
 			</a>
 		</div>

4234eb52f842ddba80717cb907836e33c6e58ac3
Tomasz Pach <tomaszpach@icloud.com>
2016-02-23 09:47:13 +0100

    single update || title text removed inline styles || 404.php

---
+++ wp-content/themes/Horizon/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomasz
+ * Date: 23/02/16
+ * Time: 09:40
+ */
+
+get_header();
+;?>
+
+<h2 class="text-md-center">404</h2>
+
+
+<?php get_footer(); ?>

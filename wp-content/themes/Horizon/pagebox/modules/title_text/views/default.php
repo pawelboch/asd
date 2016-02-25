12ce700eedbc5acc57ca2dd7359fbbf941c002c8
Tomasz Pach <tomaszpach@icloud.com>
2016-02-25 10:03:43 +0100

    title_text module added if title

--- wp-content/themes/Horizon/pagebox/modules/title_text/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/title_text/views/default.php
@@ -6,9 +6,11 @@
 
 <div class="container <?php echo $this->get('css_class'); ?>">
     <div class="col-md-12">
+        <?php if($this->get('title') != ''): ?>
         <h2 class="title">
             <?php echo $this->get('title') ;?>
         </h2>
+        <?php endif ;?>
 
         <div class="description">
             <?php echo $this->get('description') ;?>

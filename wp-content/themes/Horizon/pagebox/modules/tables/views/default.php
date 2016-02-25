0d83c40735223506c83b681a30515232a8581540
Szymek <karmonek@gmail.com>
2016-02-25 12:55:20 +0100

    Tables module modification - change to foreach loop

--- wp-content/themes/Horizon/pagebox/modules/tables/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/tables/views/default.php
@@ -8,38 +8,45 @@
 	<div class="tabs">
 		<div class="row">
 			<div class="head">
-				<div id="first" class="title col-md-3 col-sm-3 col-xs-12 active">
-					<h2><?php echo $this->get( 'first_tab_title' ) ;?></h2>
-				</div>
-				<div id="second" class="title col-md-3 col-sm-3 col-xs-12">
-					<h2><?php echo $this->get( 'second_tab_title' ) ;?></h2>
-				</div>
-				<div id="third" class="title col-md-3 col-sm-3 col-xs-12">
-					<h2><?php echo $this->get( 'third_tab_title' ) ;?></h2>
-				</div>
-				<div id="fourth" class="title col-md-3 col-sm-3 col-xs-12">
-					<h2><?php echo $this->get( 'fourth_tab_title' ) ;?></h2>
-				</div>
+				<?php
+					$tabSlug = ['first','second', 'third', 'fourth'];
+					$active = false;
+					foreach($tabSlug as $tab):
+						if($this->get( $tab.'_tab_title' )):
+				?>
+							<div id="<?php echo $tab; ?>" class="title col-md-3 col-sm-3 col-xs-12 <?php echo (!$active) ? 'active' : ''?>">
+								<h2><?php echo $this->get( $tab.'_tab_title' ) ;?></h2>
+							</div>
+				<?php
+							if(!$active):
+								$active = true;
+							endif;
+						endif;
+					endforeach;
+				?>
 				<div class="navigation-arrows">
 					<img class="arrow navigation-up" src="<?php echo get_template_directory_uri(); ?>/assets/images/upArrow.svg" alt="">
 					<img class="arrow navigation-down" src="<?php echo get_template_directory_uri(); ?>/assets/images/downArrow.svg" alt="">
 				</div>
 			</div>
-			<div class="content first active">
-				<div class="top">
+			<?php
+				$active = false;
+				foreach($tabSlug as $tab):
+					if($this->get( $tab.'_tab_title' )):
+					?>
+						<div class="content <?php echo $tab; ?> <?php echo (!$active) ? 'active' : ''?>">
+							<div class="top">
 
-				</div>
-				<?php echo $this->get( 'first_tab_content' ) ;?>
-			</div>
-			<div class="content second">
-				<?php echo $this->get( 'second_tab_content' ) ;?>
-			</div>
-			<div class="content third">
-				<?php echo $this->get( 'third_tab_content' ) ;?>
-			</div>
-			<div class="content fourth">
-				<?php echo $this->get( 'fourth_tab_content' ) ;?>
-			</div>
+							</div>
+							<?php echo $this->get( $tab.'_tab_content' ) ;?>
+						</div>
+					<?php
+						if(!$active):
+							$active = true;
+						endif;
+					endif;
+				endforeach;
+			?>
 		</div>
 	</div>
 </div>

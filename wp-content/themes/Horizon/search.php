11151e5dcf1313bdb0d8ca99296c34aa47a9726c
Szymek <karmonek@gmail.com>
2016-02-25 09:25:14 +0100

    Fix problem with menu on 992 width and added search functionality

--- wp-content/themes/Horizon/search.php
+++ wp-content/themes/Horizon/search.php
@@ -12,27 +12,21 @@ get_header(); ?>
 
 <section id="primary" class="content-area">
     <div id="content" class="site-content" role="main">
-
         <?php if ( have_posts() ) : ?>
 
             <header class="page-header">
                 <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'shape' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
             </header><!-- .page-header -->
 
-            <?php shape_content_nav( 'nav-above' ); ?>
-
-            <?php /* Start the Loop */ ?>
             <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php get_template_part( 'content', 'search' ); ?>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
             <?php endwhile; ?>
 
-            <?php shape_content_nav( 'nav-below' ); ?>
-
         <?php else : ?>
 
-            <?php get_template_part( 'no-results', 'search' ); ?>
+<!--            --><?php //get_template_part( 'no-results', 'search' ); ?>
 
         <?php endif; ?>
 

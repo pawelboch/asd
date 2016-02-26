aeb67f6fbee9db60f5091f1d6ba31f8d8958fee7
Tomasz Pach <tomaszpach@icloud.com>
2016-02-26 13:36:11 +0100

    logo + menu

--- wp-content/themes/Horizon/header.php
+++ wp-content/themes/Horizon/header.php
@@ -15,35 +15,37 @@
 		<header class="banner-header" role="banner">
 			<div class="top-header">
 				<div class="container">
-					<div class="logo">
-						<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/header_logo.svg" /></a>
-					</div>
-					<div class="navigation">
-						<div class="main-nav hidden-md-down">
-							<nav role="navigation" class="nav-main">
-								<?php
-								//$walker = new Menu_With_Description;
-								wp_nav_menu( array(
-										'theme_location' => 'primary',
-										'container' => false,
-										'menu_class' => 'nav',
-										'menu_id' => 'nav',
-										//'walker' => $walker
-										// Walker disabled also in function.php
-									)
-								);
-								?>
-							</nav>
-						</div> <!-- main-nav -->
-						<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<div class="search hidden-md-down">
-								<span class="search-ico"></span>
-								<div class="search-input-cont">
-									<input class="search-input" placeholder="Search..." name="s" id="s"> </input>
+					<div class="row">
+						<div class="logo">
+							<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/header_logo.svg" /></a>
+						</div>
+						<div class="navigation">
+							<div class="main-nav hidden-md-down">
+								<nav role="navigation" class="nav-main">
+									<?php
+									//$walker = new Menu_With_Description;
+									wp_nav_menu( array(
+											'theme_location' => 'primary',
+											'container' => false,
+											'menu_class' => 'nav',
+											'menu_id' => 'nav',
+											//'walker' => $walker
+											// Walker disabled also in function.php
+										)
+									);
+									?>
+								</nav>
+							</div> <!-- main-nav -->
+							<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+								<div class="search hidden-md-down">
+									<span class="search-ico"></span>
+									<div class="search-input-cont">
+										<input class="search-input" placeholder="Search..." name="s" id="s"> </input>
+									</div>
 								</div>
-							</div>
-						</form>
-					</div> <!-- navigation -->
+							</form>
+						</div> <!-- navigation -->
+					</div>
 					<div class="mobile-nav hidden-lg-up">
 						<div class="nav-toggle">
 							<div class="separator">

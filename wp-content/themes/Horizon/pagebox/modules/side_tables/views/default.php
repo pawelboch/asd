1c5de4e859164de74eb06dfd3750845fbb01d055
Tomasz Pach <tomaszpach@icloud.com>
2016-02-28 17:41:50 +0100

    slider_table cleanup

--- wp-content/themes/Horizon/pagebox/modules/side_tables/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/side_tables/views/default.php
@@ -25,6 +25,7 @@
 					endif; // $this->get( $tab.'_tab_title' )
 				endforeach; // $tabSlug as $tab
 				?>
+
 				<div class="navigation-arrows">
 					<img class="arrow navigation-up" src="<?php echo get_template_directory_uri(); ?>/assets/images/upArrow.svg" alt="">
 					<img class="arrow navigation-down" src="<?php echo get_template_directory_uri(); ?>/assets/images/downArrow.svg" alt="">
@@ -33,53 +34,53 @@
 
 
 			<div class="content first active">
+
 				<?php if(!empty($this->get( 'first_tab_content' ))): ?>
+
 				<div class="top">
 					<?php echo $this->get( 'first_tab_content' ) ;?>
 				</div>
-				<?php endif; ?>
-				<?php
+
+				<?php endif;
 					$sub_nav = $this->get( 'first_sub_nav' );
 					if ($sub_nav):
 
 
-			foreach( $sub_nav as $i => $sub ):
-			if(!empty($sub->sub) && !empty($sub->content)):
-			?>
+				foreach( $sub_nav as $i => $sub ):
+				if(!empty($sub->sub) && !empty($sub->content)): ?>
+
 				<div class="left col-md-8">
 					<div class="sub-menu clearfix">
 						<div id="single-<?php echo $i ;?>" class="single-menu col-md-3">
 							<?php echo $sub->sub ;?>
 						</div>
-						<?php
-						foreach ( $sub_nav as $i => $cont ):
+
+						<?php foreach ( $sub_nav as $i => $cont ):
 							if(!empty($cont->sub) && !empty($cont->content)): ?>
+
 							<div class="content sub-content single-<?php echo $i ;?>">
 								<?php echo $cont->content ;?>
 							</div>
-						<?php
-							endif; // !empty($cont->sub) && !empty($cont->content)
-						endforeach; // $sub_nav as $i => $cont
-						?>
+
+						<?php endif; // !empty($cont->sub) && !empty($cont->content)
+						endforeach; // $sub_nav as $i => $cont ?>
+
 					</div>
 				</div>
-			<?php
-			endif; // $sub_nav
+
+			<?php endif; // $sub_nav
 			endforeach; // $sub_nav as $i => $sub
-			endif // !empty($sub->sub) && !empty($sub->content)
-			;?>
-				<?php
-						foreach ( $sub_nav as $i => $side ):
-							if(!empty($side->sub) && !empty($side->content) && !empty($side->sidebar)) :
-						?>
-								<div class="sidebar col-md-4 single-<?php echo $i ;?>">
-									<img src="../../../wp-content/themes/Horizon/assets/images/sidebar_doc.png">
-									<?php echo ($side->sidebar) ? $side->sidebar : ''?>
-								</div>
-						<?php
-							endif;
-						endforeach;
-						?>
+			endif; // !empty($sub->sub) && !empty($sub->content)
+
+				foreach ( $sub_nav as $i => $side ):
+					if(!empty($side->sub) && !empty($side->content) && !empty($side->sidebar)): ?>
+						<div class="sidebar col-md-4 single-<?php echo $i ;?>">
+							<img src="../../../wp-content/themes/Horizon/assets/images/sidebar_doc.png">
+							<?php echo ($side->sidebar) ? $side->sidebar : ''?>
+						</div>
+					<?php endif;
+				endforeach; ?>
+
 			</div>
 
 
@@ -227,8 +228,6 @@
 				endif;
 				?>
 			</div>
-			
-			
 		</div>
 	</div>
 </div>

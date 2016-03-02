831b762a95e98d19ef85627a285e610fdaaaedfc
Piotr Grzesiak <piter2k1@gmail.com>
2016-03-02 15:39:35 +0100

    HTML structure updated

--- wp-content/themes/Horizon/header.php
+++ wp-content/themes/Horizon/header.php
@@ -4,8 +4,8 @@
 		<title><?php wp_title('|', true, 'right'); ?></title>
 		<meta content="index, follow" name="robots">
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<?php wp_head(); ?>

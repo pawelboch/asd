8378d662187010b2a65feefed6cff0b02beda72d
Tomasz Pach <tomaszpach@icloud.com>
2016-03-04 16:00:28 +0100

    Header fonts || sidetable fonts || test template

---
+++ wp-content/themes/Horizon/templates/test-template.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+Template Name: test template
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<!--<style>-->
+<!--	body {-->
+<!--		height: 1200px;-->
+<!--	}-->
+<!--	header {-->
+<!--		position: relative;-->
+<!--		display: none;-->
+<!--	}-->
+<!--	footer {-->
+<!--		display: none;-->
+<!--	}-->
+<!--	.parallax-window {-->
+<!--		/*background-image: url("/wp-content/themes/Horizon/assets/images/background.jpg");*/-->
+<!--		height: 800px;-->
+<!--	}-->
+<!--</style>-->
+
+<?php pagebox() ;?>
+
+<!--	<div class="parallax-window" data-parallax="scroll" data-image-src="/wp-content/themes/Horizon/assets/images/image-2.png"></div>-->
+
+<?php get_footer(); ?>

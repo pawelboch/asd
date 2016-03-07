b0d3518c5268f022fb0df18919ad33aea4fcd05b
Tomasz Pach <tomaszpach@icloud.com>
2016-03-07 12:15:54 +0100

    two images - added option to link || side tables animation

--- wp-content/themes/Horizon/pagebox/modules/two_images/views/default.php
+++ wp-content/themes/Horizon/pagebox/modules/two_images/views/default.php
@@ -16,7 +16,7 @@
 
 				<div class="col-md-12">
 					<p class="description"><?php echo $this->get( 'description_left' ) ;?></p>
-					<a href="#" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
+					<a href="<?php echo $this->get('btn_link1'); ?>" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
 				</div>
 
 			</div>
@@ -30,7 +30,7 @@
 
 				<div class="col-md-12">
 					<p class="description"><?php echo $this->get( 'description_right' ) ;?></p>
-					<a href="#" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
+					<a href="<?php echo $this->get('btn_link2'); ?>" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
 				</div>
 			</div>
 		</div>
@@ -45,7 +45,7 @@
 
 					<div class="col-md-12">
 						<p class="description"><?php echo $this->get( 'description_left' ) ;?></p>
-						<a href="#" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
+						<a href="<?php echo $this->get('btn_link1'); ?>" class="learn-more-btn col-xs-12 col-sm-4 col-xs-6 btn-2">learn more<span></span></a>
 					</div>
 
 				</div>
